839a0878e01ebd138e92b209db7367a2c9689e75
Zepronex <callesvensson@hotmail.se>
2023-05-10 18:38:41 +0200

    Implemented the functionality for the button so that now it starts the second activity, added data to intent bundle using extras and made a TextView widget in the second activity that displays the data.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton; // Declare reference to button widget
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton); // Find button by ID
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                // Create a Bundle object and add data to it
+                Bundle bundle = new Bundle();
+                bundle.putString("message", "Passing data is cool!"); // Replace "key" and "value" with your own data
+
+                // Pass Bundle object to Intent.
+                intent.putExtras(bundle);
+
+                // Start second activity
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,12 +3,25 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // Get Bundle that was passed to this activity.
+        Bundle bundle = getIntent().getExtras();
+
+        // Extract data from Bundle.
+        String message = bundle.getString("message");
+
+        // Display message in TextView.
+        TextView messageTextView = findViewById(R.id.messageTextView);
+        messageTextView.setText(message);
     }
+
 }
